9604020a9fb792b9ab4d4635cbe1332588a00b94
VouttaRene <45213212+VouttaRene@users.noreply.github.com>
2020-11-23 13:13:08 +0100

    playing field layout

--- AndroidStudio/Mastermind/app/src/main/java/de/voutta/mastermind/StartGame/PlayGame.java
+++ AndroidStudio/Mastermind/app/src/main/java/de/voutta/mastermind/StartGame/PlayGame.java
@@ -1,10 +1,16 @@
 package de.voutta.mastermind.StartGame;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import java.util.List;
 
+import de.voutta.mastermind.MainMenu;
 import de.voutta.mastermind.R;
 
 public class PlayGame extends AppCompatActivity {
@@ -13,6 +19,19 @@ public class PlayGame extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_play_game);
+
+        //ReturnToMenu Button
+        ImageButton returnButton = findViewById(R.id.returnButton);
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(PlayGame.this, MainMenu.class);
+
+                startActivity(intent);
+            }
+        });
+
 
         CreateSoughtAfterList createSoughtAfterList = new CreateSoughtAfterList();
         createSoughtAfterList.allColorList();
